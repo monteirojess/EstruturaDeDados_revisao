e14dc3e1508be5201b4c945fae9bfc7570a30eec
Jessica Monteiro <101065281+monteirojess@users.noreply.github.com>
2022-08-17 14:49:57 -0300

    Update numerosprimos.py

--- numerosprimos.py
+++ numerosprimos.py
@@ -1,3 +1,5 @@
+# Faça um programa que gera uma lista dos números primos existentes entre 1 e um número inteiro informado pelo usuário.
+
 numero = int(input('Digite um número:'))
 lista_primos = []
 primos = ''
